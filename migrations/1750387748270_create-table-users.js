22ac48e480a6bdecb0eb4a71f66269f0d8521b73
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-20 10:37:37 +0700

    update

--- migrations/1750387748270_create-table-users.js
+++ migrations/1750387748270_create-table-users.js
@@ -1,18 +1,25 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-export const shorthands = undefined;
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {};
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const down = (pgm) => {};
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
