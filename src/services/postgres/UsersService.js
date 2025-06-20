216fa95220dc3fa46ae4a158845ab9c0aeb0e2de
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-20 11:07:51 +0700

    setup users plugin

---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,67 @@
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ fullname, username, password }) {
+    await this.verifyNewUsername(username);
+
+    const id = `users-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, fullname, username, hashedPassword],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      value: [username],
+    };
+
+    const result = this._pool.query(query);
+
+    if ((await result).rows.length) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
+    }
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, passwrod: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
+  }
+}
+
+module.exports = UsersService;
